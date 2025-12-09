55efaf400d4304f75173c1871698e13277f0b297
Stephenasembo <165886904+Stephenasembo@users.noreply.github.com>
2025-12-09 14:36:18 +0300

    Feat: Setup Tab Screens And Navigation

---
+++ app/_layout.tsx
@@ -0,0 +1,10 @@
+import { Tabs } from 'expo-router';
+
+export default function RootTabNavigator() {
+  return (
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen name="index" options={{ tabBarLabel: "Home" }}/>
+      <Tabs.Screen name="profile" options={{ tabBarLabel: "Profile" }}/>
+    </Tabs>
+  )
+}
